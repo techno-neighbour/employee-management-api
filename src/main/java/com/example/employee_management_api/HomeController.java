44c2142dd254a2ecfafabe0a81ec36003853b921
Fudail <geniusmf8@gmail.com>
2026-06-22 14:46:13 +0530

    Add Files

---
+++ src/main/java/com/example/employee_management_api/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.employee_management_api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee API is running successfully!";
+    }
+}
---
+++ src/main/java/com/example/employee_management_api/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.example.employee_management_api.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.employee_management_api.model.Employee;
+
+@Service
+public class EmployeeService {
+
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeService() {
+        employees.add(new Employee(1, "Senthur", "DevOps", 50000));
+        employees.add(new Employee(2, "John", "Testing", 45000));
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employees;
+    }
+}
